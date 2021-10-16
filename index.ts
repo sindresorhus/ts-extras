8b552f3e5269ad4bdff97dba78f139a0303e85fc
Sindre Sorhus <sindresorhus@gmail.com>
2021-10-16 22:30:42 +0700

    Add `assertError` function

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import {Mutable} from 'type-fest';
 
+const {toString} = Object.prototype;
+
 /**
 Check whether a value is defined (non-nullable), meaning it is neither `null` or `undefined`.
 
@@ -15,6 +17,39 @@ import {isDefined} from 'ts-extras';
 */
 export function isDefined<T>(value: T | null | undefined): value is T {
 	return value !== null && value !== undefined;
+}
+
+/**
+Assert that the given value is an `Error`.
+
+If the value is not an `Error`, a helpful `TypeError` will be thrown.
+
+This can be useful as any value could potentially be thrown, but in practice, it's always an `Error`. However, because of this, TypeScript makes the caught error in a try/catch statement `unknown`, which is inconvenient to deal with.
+
+@example
+```
+import {assertError} from 'ts-extras';
+
+try {
+	fetchUnicorns();
+} catch (error: unknown) {
+	assertError(error)
+
+	// `error` is now of type `Error`
+
+	if (error.message === 'Failed to fetch') {
+		retry();
+		returnl
+	}
+
+	throw error;
+}
+```
+*/
+export function assertError(value: unknown): asserts value is Error {
+	if (!(value instanceof Error || toString.call(value) === '[object Error]')) {
+		throw new TypeError(`Expected an \`Error\`, got \`${JSON.stringify(value)}\` (${typeof value})`);
+	}
 }
 
 /**
