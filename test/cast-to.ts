d10c2489965aeb4fc09a3f7db4be3da835f1986f
Sindre Sorhus <sindresorhus@gmail.com>
2021-11-01 14:01:33 +0700

    Update cast-to.ts

--- test/cast-to.ts
+++ test/cast-to.ts
@@ -7,7 +7,7 @@ test('castTo()', t => {
 
 	const add = castTo<(a: number, b: number) => number>(
 		// @ts-expect-error
-		(...args) => curryAdd(args[0])(args[1]),
+		(...arguments_) => curryAdd(arguments_[0])(arguments_[1]),
 	);
 	expectTypeOf(add).toMatchTypeOf<(a: number, b: number) => number>();
 
