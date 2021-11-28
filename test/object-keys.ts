036177dd886de3ca6bcd538be3342ef3b44e7af2
Jonah Snider <jonah@jonahsnider.com>
2021-11-28 11:30:19 -0800

    Fix return type of `objectKeys()`

--- test/object-keys.ts
+++ test/object-keys.ts
@@ -3,8 +3,8 @@ import {expectTypeOf} from 'expect-type';
 import {objectKeys} from '../source/index.js';
 
 test('objectKeys()', t => {
-	type Item = 'a' | 'b' | 'c';
-	const items = objectKeys({a: 1, b: 2, c: 3});
+	type Item = 'a' | 'b' | 'c' | '4';
+	const items = objectKeys({a: 1, b: 2, c: 3, 4: 4, [Symbol('5')]: 5});
 
 	expectTypeOf<Item[]>(items);
 	t.deepEqual(items, ['a', 'b', 'c']);
