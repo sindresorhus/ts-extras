5700c8537057249d8b858c69de8e6095e372f405
Younho Choo <yo+github@younho9.com>
2021-11-28 22:17:26 +0900

    Exclude symbol property from `objectEntries()`

--- test/object-entries.ts
+++ test/object-entries.ts
@@ -3,9 +3,13 @@ import {expectTypeOf} from 'expect-type';
 import {objectEntries} from '../source/index.js';
 
 test('objectEntries()', t => {
-	type Entry = ['a' | 'b' | 'c', number];
-	const entries = objectEntries({a: 1, b: 2, c: 3});
+	type Entry = [1 | 'stringKey', number | string];
+	const entries = objectEntries({
+		1: 123,
+		stringKey: 'someString',
+		[Symbol('symbolKey')]: true,
+	});
 
 	expectTypeOf<Entry[]>(entries);
-	t.deepEqual(entries, [['a', 1], ['b', 2], ['c', 3]]);
+	t.deepEqual(entries, [['1', 123], ['stringKey', 'someString']]);
 });
