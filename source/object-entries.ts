6d9ff540e92e9e2584f08fb676cda9d2e17bbdb1
Sindre Sorhus <sindresorhus@gmail.com>
2021-11-29 17:37:49 +0700

    Update object-entries.ts

--- source/object-entries.ts
+++ source/object-entries.ts
@@ -1,7 +1,7 @@
 /**
 A strongly-typed version of `Object.entries()`.
 
-This is useful since `Object.entries()` always returns an array of Array<[string, T]>. This function returns a strongly-typed array of the entries of the given object.
+This is useful since `Object.entries()` always returns an array of `Array<[string, T]>`. This function returns a strongly-typed array of the entries of the given object.
 
 - [TypeScript issues about this](https://github.com/microsoft/TypeScript/pull/12253)
 
@@ -9,8 +9,11 @@ This is useful since `Object.entries()` always returns an array of Array<[string
 ```
 import {objectEntries} from 'ts-extras';
 
-const stronglyTypedEntries = objectEntries({a: 1, b: 2, c: 3}); // =>  Array<["a" | "b" | "c", number]>
-const untypedEntries = Object.entries(items); // => Array<[string, number]>
+const stronglyTypedEntries = objectEntries({a: 1, b: 2, c: 3});
+//=> Array<['a' | 'b' | 'c', number]>
+
+const untypedEntries = Object.entries(items);
+//=> Array<[string, number]>
 ```
 */
 export function objectEntries<Type extends Record<PropertyKey, unknown>, Key extends `${Exclude<keyof Type, symbol>}`>(value: Type): Array<[Key, Type[Key]]> {
