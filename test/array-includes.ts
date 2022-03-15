6b85d8ed2b6b5821685be1952b946cdae3f9b330
Jiajun Chen <tychenjiajun@live.cn>
2022-03-15 10:01:31 +0800

    test: add test for setHas and arrayIncludes

--- test/array-includes.ts
+++ test/array-includes.ts
@@ -1,14 +1,22 @@
 import test from 'ava';
+import {expectTypeOf} from 'expect-type';
 import {arrayIncludes} from '../source/index.js';
 
 test('arrayIncludes()', t => {
 	const values = ['a', 'b', 'c'] as const;
 	const validValue: unknown = 'a';
 	const invalidValue: unknown = 'd';
+	const invalidTypedValue = 1;
 	let testValueType: typeof values[number];
+
+	expectTypeOf(values).items.toMatchTypeOf<typeof validValue>();
+	expectTypeOf(values).items.toMatchTypeOf<typeof invalidValue>();
+	expectTypeOf(values).items.not.toMatchTypeOf<typeof invalidTypedValue>();
 
 	t.true(arrayIncludes(values, validValue));
 	t.false(arrayIncludes(values, invalidValue));
+	// @ts-expect-error
+	t.false(arrayIncludes(values, invalidTypedValue));
 
 	if (arrayIncludes(values, validValue)) {
 		// @ts-expect-error
--- test/set-has.ts
+++ test/set-has.ts
@@ -1,4 +1,5 @@
 import test from 'ava';
+import {expectTypeOf} from 'expect-type';
 import {setHas} from '../source/index.js';
 
 test('setHas()', t => {
@@ -6,10 +7,17 @@ test('setHas()', t => {
 	const valueSet = new Set(values);
 	const validValue: unknown = 'a';
 	const invalidValue: unknown = 'd';
+	const invalidTypedValue = 1;
 	let testValueType: typeof values[number];
+
+	expectTypeOf(values).items.toMatchTypeOf<typeof validValue>();
+	expectTypeOf(values).items.toMatchTypeOf<typeof invalidValue>();
+	expectTypeOf(values).items.not.toMatchTypeOf<typeof invalidTypedValue>();
 
 	t.true(setHas(valueSet, validValue));
 	t.false(setHas(valueSet, invalidValue));
+	// @ts-expect-error
+	t.false(setHas(valueSet, invalidTypedValue));
 
 	// eslint-disable-next-line unicorn/prefer-ternary
 	if (setHas(valueSet, validValue)) {
